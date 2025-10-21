28453fb5bc24f2d083c5c59e6888a160552425cc
Florian.Lindinger <florianlindinger26@gmail.com>
2025-10-21 18:16:10 +0200

    created useful batch function file

---
+++ useful batch functions.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+:: ===================
+:: ==== Test Area ====
+:: ===================
+
+
+
+
+
+:: ====================
+:: ==== Functions: ====
+:: ====================
+EXIT /B
+
+:: =================================================
+:: function that makes relative path (relative to current working directory) to absolute if not already. Works for empty path (relative) path:
+:: Usage:
+::    call :make_absolute_path_if_relative "%a_path%"
+::    set "abs_path=%output%"
+:: =================================================
+:make_absolute_path_if_relative
+    if "%~1"=="" (
+        set "OUTPUT=%CD%"
+    ) else (
+	    set "OUTPUT=%~f1"
+    )
+	goto :EOF
+:: =================================================
