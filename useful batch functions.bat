f099818321a2d7af5f0d5fb16307181c90d1957f
Florian.Lindinger <florianlindinger26@gmail.com>
2025-10-21 18:16:32 +0200

    update comment

--- useful batch functions.bat
+++ useful batch functions.bat
@@ -13,7 +13,7 @@
 EXIT /B
 
 :: =================================================
-:: function that makes relative path (relative to current working directory) to absolute if not already. Works for empty path (relative) path:
+:: function that makes relative path (relative to current working directory) to :: absolute if not already. Works for empty path (relative) path:
 :: Usage:
 ::    call :make_absolute_path_if_relative "%a_path%"
 ::    set "abs_path=%output%"
