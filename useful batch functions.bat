264ce6a807700b70aa817ddc90f78989706e8b93
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-02 01:11:51 +0100

    added prompt_user

--- useful batch functions.bat
+++ useful batch functions.bat
@@ -13,12 +13,12 @@ EXIT /B
 :: ==== Functions: ====
 :: ====================
 
-:: =================================================
+::::::::::::::::::::::::::::::::::::::::::::::::
 :: function that makes relative path (relative to current working directory) to :: absolute if not already. Works for empty path (relative) path:
 :: Usage:
 ::    call :make_absolute_path_if_relative "%some_path%"
 ::    set "abs_path=%output%"
-:: =================================================
+::::::::::::::::::::::::::::::::::::::::::::::::
 :make_absolute_path_if_relative
     if "%~1"=="" (
         set "OUTPUT=%CD%"
@@ -26,4 +26,20 @@ EXIT /B
 	    set "OUTPUT=%~f1"
     )
 goto :EOF
+:: =================================================
+:: =================================================
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: function that prompts user with "Enter y/n for Yes/No" and sets OUTPUT=1 for y and OUTPUT=0 for n.
+::::::::::::::::::::::::::::::::::::::::::::::::
+:prompt_user
+	CHOICE /c YN /m Delete? Enter y/n for Yes/No
+	IF %ERRORLEVEL%==1 (
+		set "OUTPUT=1"
+	) else (
+		set "OUTPUT=0"
+	)
+GOTO :EOF
+:: =================================================
 :: =================================================
