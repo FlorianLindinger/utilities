8750ade553c4b54b4c3fcf71b370b75dcc277797
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-11 22:24:48 +0100

    added readme to change icon; ruff .py file

--- change window icon/change_icon.py
+++ change window icon/change_icon.py
@@ -1,30 +1,29 @@
-# Usage: 
+# Usage:
 # arg1:          {window name}
 # arg2(optional) {icon_path}
 # arg3(optional) {anything if you want errors to print}
 
-import time
-import win32api  # install as pywin32
-import win32gui  # install as pywin32
-import win32con  # install as pywin32
 import sys
+import time
+
+import win32api  # install as pywin32
+import win32con  # install as pywin32
+import win32gui  # install as pywin32
 
 
 def set_window_icon(window_title: str, icon_path: str, print_warning=False):
     try:
         hwnd = win32gui.FindWindow(None, window_title)
         icon_flags = win32gui.LR_LOADFROMFILE | win32gui.LR_DEFAULTSIZE
-        hicon = win32gui.LoadImage(
-            None, icon_path, win32gui.IMAGE_ICON, 0, 0, icon_flags)
-        win32api.SendMessage(hwnd, win32con.WM_SETICON,
-                             win32con.ICON_SMALL, hicon)
-        win32api.SendMessage(hwnd, win32con.WM_SETICON,
-                             win32con.ICON_BIG, hicon)
+        hicon = win32gui.LoadImage(None, icon_path, win32gui.IMAGE_ICON, 0, 0, icon_flags)
+        win32api.SendMessage(hwnd, win32con.WM_SETICON, win32con.ICON_SMALL, hicon)
+        win32api.SendMessage(hwnd, win32con.WM_SETICON, win32con.ICON_BIG, hicon)
     except Exception as e:
         if print_warning:
             print(e)
             print(
-                "Error: Use change_icon.exe with command:\nchange_icon {window name} {icon_path} {anything_if_you_want_to_print_errors}")
+                "Error: Use change_icon.exe with command:\nchange_icon {window name} {icon_path} {anything_if_you_want_to_print_errors}"
+            )
 
 
 if len(sys.argv) == 3:
@@ -37,4 +36,5 @@ elif len(sys.argv) == 4:
     time.sleep(0.2)
 else:
     print(
-        "Error: Use change_icon.exe with command:\nchange_icon {window name} {icon_path} {anything_if_you_want_to_print_errors}")
+        "Error: Use change_icon.exe with command:\nchange_icon {window name} {icon_path} {anything_if_you_want_to_print_errors}"
+    )
---
+++ change window icon/readme.txt
@@ -0,0 +1,3 @@
+if you start the exe with
+start "" /min change_icon_exe "%program_name%" "%icon_path%"
+if won't wait for completion which can take ~1ss
