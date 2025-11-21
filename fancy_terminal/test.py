eb0b891bfe7c299f2d7db0dfc858103ea1edf6f6
Florian.Lindinger <florianlindinger26@gmail.com>
2025-11-21 15:24:39 +0100

    update todo

--- fancy_terminal/test.py
+++ fancy_terminal/test.py
@@ -10,13 +10,11 @@ while True:
 #todo
 # #=====================================
 
-# add docstring to everything
+# slight Animations especially for moving to system tray
 
-# send_input?
+#chsnge font with ctrl +- and scroll and. buttons 
 
-# side snap does not remember
-
-# make user command blue
+#clwar button 
 
 # mkae option to remove or add timestaps to all orints. make it removeber timesatap eve n if not tunred on
 
@@ -28,9 +26,38 @@ while True:
 
 # prevent window from going too low to get back. apparently Windows makes it snap up
 
-# make it use windows terminal icon if fallback on not there
-
 # dragging away from max makes it scale instaed of snap to old size and drag
+
+#Option to print to file parallel and link to that file
+
+#Calculator option?
+
+#Option to open logged file for crash
+
+#Can I right click paste and copy?
+
+#Auto clickable links and paths?
+
+#Print of name in asci Option
+
+#Stop python crash and print and give options to restart? Open windows prompt for crash
+
+#transparency option 
+
+#----------------
+# interactions
+#--------------
+
+#Option to remember last state? Safe history? Relaod?
+
+#Maybe tabs for chat and for plots
+
+#Does progressbars work? Maybe build one in
+
+#Give option to change color of taskbar icon during runtime and before. tell user how to change icon and name during runtime and also color
+
+#any way of python to communicate with terminal. for like buttons.
+#Help button giving all commands
 
 # #=====================================
 # settings
@@ -48,6 +75,9 @@ while True:
 # end fix
 # #=====================================
 
+#Add as option to PyApp-Template
+
+#How does it abfangen commands that do terminal stuff
 
 # remove unneeded args
 
