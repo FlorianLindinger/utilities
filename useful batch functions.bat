0bbc5155cd616d128402c3fb4b676f71c4fec298
Florian.Lindinger <florianlindinger26@gmail.com>
2025-12-01 18:31:38 +0100

    update gitignotre and batch function collection

--- useful batch functions.bat
+++ useful batch functions.bat
@@ -59,3 +59,34 @@ GOTO :EOF
 :: =================================================
 :: =================================================
 
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: function to call twice to get printed the duration between the two calls
+::::::::::::::::::::::::::::::::::::::::::::::::
+:count_duration
+setlocal enabledelayedexpansion
+rem %TIME% ? HH:MM:SScc by removing the comma
+set "t=%time:,=%"
+rem HH = characters 0–1
+set "HH=!t:~0,2!"
+rem MM = characters 3–4
+set "MM=!t:~3,2!"
+rem SS = characters 6–7
+set "SS=!t:~6,2!"
+rem CC = characters 9–2 (centiseconds)
+set "CC=!t:~9,2!"
+rem calculate centiseconds since midnight
+set /a total=(HH*3600 + MM*60 + SS)*100 + CC
+REM set global variable to current time if unset or print time passed since start if already set and reset afterwards
+if "%count_time_s_start%"=="" (
+    endlocal & set "count_time_s_start=%total%"
+) else (
+    set /a diff=%total%-%count_time_s_start%
+    if !diff! lss 0 set /a diff+=24*60*60*100
+    set /a SEC=diff/100
+    set /a CS=diff%%100
+    echo Passed Time: !SEC!.!CS! s
+    endlocal & set "count_time_s_start="
+)
+exit /b 0
+:: =================================================
+:: =================================================
