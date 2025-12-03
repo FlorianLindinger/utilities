e2ac5e693b181e59b70ff277aac0be46e76ba757
Florian.Lindinger <florianlindinger26@gmail.com>
2025-12-03 20:32:53 +0100

    upgraded abs path function

--- useful batch functions.bat
+++ useful batch functions.bat
@@ -1,11 +1,14 @@
-@ECHO OFF
+@echo off & setlocal enabledelayedexpansion
 :: ===================
 :: ==== Test Area ====
 :: ===================
 
+:: PUT TESTS HERE
 
-
-
+:: ===================
+echo.
+echo Press any key to exit.
+pause > nul
 EXIT /B
 :: ===================
 
@@ -14,21 +17,25 @@ EXIT /B
 :: ====================
 
 ::::::::::::::::::::::::::::::::::::::::::::::::
-:: function that makes relative path (relative to current working directory) to :: absolute if not already. Works for empty path (relative) path:
+:: function that converts relative (to current working directory) path {arg1} to absolute and sets it to variable {arg2}. Works for empty path {arg1} which then sets the current working directory to variable {arg2}. Raises error if {arg2} is missing:
 :: Usage:
-::    call :make_absolute_path_if_relative "%some_path%"
-::    set "abs_path=%output%"
+::    call :set_abs_path "%some_path%" "some_path"
 ::::::::::::::::::::::::::::::::::::::::::::::::
-:make_absolute_path_if_relative
+:set_abs_path
+    if "%~2"=="" (
+        echo [Error] Second argument is missing for :set_abs_path function in "%~f0". (First argument was "%~1"^). 
+        echo Aborting. Press any key to exit.
+        pause > nul
+        exit /b 1
+    )
     if "%~1"=="" (
-        set "OUTPUT=%CD%"
+        set "%~2=%CD%"
     ) else (
-	    set "OUTPUT=%~f1"
+	    set "%~2=%~f1"
     )
 goto :EOF
-:: =================================================
-:: =================================================
 
+:: =================================================
 
 ::::::::::::::::::::::::::::::::::::::::::::::::
 :: function that prompts user with "Enter y/n for Yes/No" and sets OUTPUT=1 for y and OUTPUT=0 for n.
@@ -41,7 +48,7 @@ goto :EOF
 		set "OUTPUT=0"
 	)
 GOTO :EOF
-:: =================================================
+
 :: =================================================
 
 ::::::::::::::::::::::::::::::::::::::::::::::::
@@ -56,23 +63,23 @@ IF %ERRORLEVEL%==0 (
 	SET "OUTPUT=0"
 )
 GOTO :EOF
-:: =================================================
+
 :: =================================================
 
 ::::::::::::::::::::::::::::::::::::::::::::::::
-:: function to call twice to get printed the duration between the two calls
+:: function to call twice to get printed the duration between the two calls. Arg for second call gives the text before print of e.g. "{arg1} 18.2 s" ({arg1} default: "Duration:").
 ::::::::::::::::::::::::::::::::::::::::::::::::
 :count_duration
 setlocal enabledelayedexpansion
 rem %TIME% ? HH:MM:SScc by removing the comma
 set "t=%time:,=%"
-rem HH = characters 0–1
+rem HH = characters 0ï¿½1
 set "HH=!t:~0,2!"
-rem MM = characters 3–4
+rem MM = characters 3ï¿½4
 set "MM=!t:~3,2!"
-rem SS = characters 6–7
+rem SS = characters 6ï¿½7
 set "SS=!t:~6,2!"
-rem CC = characters 9–2 (centiseconds)
+rem CC = characters 9ï¿½2 (centiseconds)
 set "CC=!t:~9,2!"
 rem calculate centiseconds since midnight
 set /a total=(HH*3600 + MM*60 + SS)*100 + CC
@@ -84,9 +91,32 @@ if "%count_time_s_start%"=="" (
     if !diff! lss 0 set /a diff+=24*60*60*100
     set /a SEC=diff/100
     set /a CS=diff%%100
-    echo Passed Time: !SEC!.!CS! s
+    if "%~1"=="" ( set "text=Duration:"
+    ) else ( set "text=%~1" )
+    echo !text! !SEC!.!CS! s
     endlocal & set "count_time_s_start="
 )
 exit /b 0
+
 :: =================================================
+
+::::::::::::::::::::::::::::::::::::::::::::::::
+:: function that prints e.g. "{arg2} 40.0 MB" ({arg2} default: "Size:") for first arg = file or folder path. Converts to approprate GB/MB/...
+::::::::::::::::::::::::::::::::::::::::::::::::
+:print_size
+setlocal enabledelayedexpansion
+rem %1 = path to the file or folder
+set "item_path=%~1"
+if not exist "%item_path%" (
+    echo File/Folder not found for size determination: %item_path%
+    exit /b 1
+)
+if "%~2"=="" ( set "text=Size:"
+) else ( set "text=%~2" )
+FOR /F "usebackq tokens=*" %%G IN (`powershell -ExecutionPolicy Bypass -Command "$Path = '%item_path%'; $Item = Get-Item -LiteralPath $Path; $1MB = 1048576; $1GB = 1073741824; if ($Item.PSIsContainer) { $B = (Get-ChildItem -Recurse -File -LiteralPath $Path | Measure-Object -Sum Length).Sum; } else { $B = $Item.Length; } if (-not $B) { $B = 0 }; if ($B -ge $1GB) { '{0:N1} GB' -f ($B / $1GB) } elseif ($B -ge $1MB) { '{0:N1} MB' -f ($B / $1MB) } else { '{0:N0} Bytes' -f $B }"`) DO (
+    echo %text% %%G 
+)
+endlocal
+exit /b 0
+
 :: =================================================
